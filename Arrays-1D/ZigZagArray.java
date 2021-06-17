27a0e52571bff2913633cb53ef00b731c11db648
Bhavye jain <bhavyejain001@gmail.com>
2021-06-17 21:45:15 +0530

    Two-Pointers Algo

---
+++ Arrays-1D/ZigZagArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ZigZagArray {
+	
+	public static void main(String[] args) {
+		
+		int arr[]= {4,3,7,8,6,2,1};
+		boolean flag = true;
+		int temp=0;
+		
+		for(int i=0 ;i < arr.length-1;i++) {
+			if(flag) {
+			if(arr[i] > arr[i+1] ) {
+				temp=arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]=temp;
+				}
+			}else{
+				if(arr[i] < arr[i+1]){
+					temp=arr[i];
+					arr[i]=arr[i+1];
+					arr[i+1]=temp;
+				}
+			}	
+		flag = !flag;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
