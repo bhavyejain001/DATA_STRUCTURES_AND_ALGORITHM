4fd9a32cc49c11f58fbc713f68f3ad630bdb1866
Bhavye jain <bhavyejain001@gmail.com>
2021-07-08 21:30:47 +0530

    LinkedList Implementation

--- LinkedList/LinkedListImplementation/LinkedListFunctions.java
+++ LinkedList/LinkedListImplementation/LinkedListFunctions.java
@@ -9,7 +9,9 @@ class Node<T>{
 }
 
 public class LinkedListFunctions<T> {
+	
 	Node<T> start;
+	
 	void addAtBeg(Node<T> node) {
 		if(start == null) {
 			start = node;
@@ -18,21 +20,130 @@ public class LinkedListFunctions<T> {
 			start = node;
 		}
 	}
-	
+	 
+	void addAtEnd(Node<T> node) {
+		if(start == null) {
+			start = node;
+		}else {
+			Node<T> temp = start;
+			while(temp.next!=null) {
+				temp = temp.next;
+			}
+			temp.next = node;
+		}
+	}
 	void addByPos(Node<T> node, int pos) {
-		if(pos == 0) {
+		if(pos == 1) {
 			node.next = start;
 			start = node;
 		}
 		int i =1;
 		Node<T> temp = start;
-		while(i <= pos) {
+		while(i < pos-1) {
 			temp = temp.next;
 			i++;
 		}
-		node.next = temp.next;
+		Node<T> curr = temp.next;
+		node.next = curr;
 		temp.next = node;
 	}
+	
+	void deleteAtEnd() {
+		if(start == null) {
+			System.out.println("ll is Empty");
+			return;
+		}
+		if(start.next == null) {
+			start = null ;
+			System.out.println("Node Deleted");
+			return;
+		}else {
+			Node<T> curr = start;
+			Node<T> prev = null ;
+			while(curr.next!=null) {
+				prev = curr;
+				curr = curr.next;
+			}
+			prev.next=null;
+		}
+	}
+	
+	void deleteByPos(int pos) {
+		if(pos == 1) {
+			start = start.next;
+		}
+		int i=1;
+		Node<T> prev = start;
+		while( i < pos-1) {
+			prev = prev.next;
+			i++;
+		}
+		Node<T> curr = prev.next;
+		prev.next = curr.next;
+	}
+	
+	void deleteAtBegin() {
+		if(start == null || start.next == null) {
+			start = null;
+			System.out.println("Node Deleted");
+			return;
+		}
+		Node<T> temp = start;
+		start = start.next;
+		temp.next = null;
+ 	}
+	
+	void midPoint() {
+		Node<T> slow = start;
+		Node<T> fast = start;
+		if(start!= null) {
+			while(slow!=null && fast.next!=null) {
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+			System.out.println("MidPoint is: "+slow.data);
+		}
+	}
+	
+	int size() {
+		Node<T> temp = start;
+		int count = 0;
+		while(temp!=null) {
+			temp = temp.next;
+			count++;
+		}
+		return count;
+	}
+	
+	Node<T> get(int index){
+		int size = this.size();
+		if(size == 0) {
+			throw new RuntimeException("LL is empty");
+		}
+		if(index < 0 || index >= size) {
+			throw new RuntimeException("Invalid Index");
+		}
+		Node<T> temp = start;
+		for(int i = 0 ;i < index ;i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+	
+	void reverse() {
+		int left = 0;
+		int right = size() - 1;
+		while(left < right) {
+			Node leftNode = get(left);
+			Node rightNode = get(right);
+			Object temp =  leftNode.data;
+			leftNode.data = rightNode.data;
+			rightNode.data = temp;
+			left++;
+			right--;
+		}
+	}
+	
 	void print() {
 		Node<T> temp = start;
 		while(temp!=null) {
@@ -41,4 +152,5 @@ public class LinkedListFunctions<T> {
 		}
 		System.out.println("null");
 	}
+
 }
--- LinkedList/LinkedListImplementation/MainFunction.java
+++ LinkedList/LinkedListImplementation/MainFunction.java
@@ -10,14 +10,19 @@ public class MainFunction {
 			System.out.println("1. Add the Node");
 			System.out.println("2. Print the Node");
 			System.out.println("3. Add At Position");
+			System.out.println("4. Delete the Last Node");
+			System.out.println("5. Delete At Position");
+			System.out.println("6. Delete At Begining");
+			System.out.println("7. Find the MidPoint");
+			System.out.println("8. Count the Nodes");
+			System.out.println("9. Reverse the LinkedList");
 			int choice = scanner.nextInt();
-			Node<Integer> node;
 			switch(choice) {
 			case 1:
 				System.out.println("Enter the data");
 				int data = scanner.nextInt();
-				 node = new Node<>(data);
-				opr.addAtBeg(node);
+				Node<Integer> node; node = new Node<>(data);
+				opr.addAtEnd(node);
 				break;
 			case 2:
 				opr.print();
@@ -29,6 +34,27 @@ public class MainFunction {
 				int pos = scanner.nextInt();
 				node = new Node<>(data1);
 				opr.addByPos(node, pos);
+			case 4:
+				opr.deleteAtEnd();
+				break;
+			case 5:
+				System.out.println("Enter the Index");
+				int index = scanner.nextInt();
+				opr.deleteByPos(index);
+				break;
+			case 6:
+				opr.deleteAtBegin();
+				break;
+			case 7:
+				opr.midPoint();
+				break;
+			case 8:
+				System.out.println("Count is: "+opr.size());
+				break;
+			case 9:
+				opr.reverse();
+				opr.print();
+				break;
 			}
 		}
 	}
